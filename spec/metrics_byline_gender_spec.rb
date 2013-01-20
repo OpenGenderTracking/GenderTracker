c4e93250caaf82eec6f4fa08d4f154c505a05178
Irene Ros <irene@bocoup.com>
2013-01-20 17:14:26 +0000

    Added byline gender + spec

---
+++ spec/metrics_byline_gender_spec.rb
@@ -0,0 +1,82 @@
+require "spec_helper"
+require 'json'
+
+require File.expand_path(File.join(File.dirname(__FILE__), "../src/article"))
+require File.expand_path(File.join(File.dirname(__FILE__), "../src/metrics/metric.rb"))
+require File.expand_path(File.join(File.dirname(__FILE__), "../src/metrics/byline_gender.rb"))
+
+describe "Metrics::BylineGender" do
+
+  context "initialisation" do
+    it "should have a name" do
+      Metrics::BylineGender.get_name.should eq "bylineGender"
+    end
+
+    it "should accept no parameters" do
+      mn = Metrics::BylineGender.new()
+      mn.should be_a Metrics::BylineGender
+    end
+  end
+
+  context "process" do
+
+    before {
+      @mn = Metrics::BylineGender.new()
+      @female_article = Article.new(File.join(
+      FIXTURES_DIR, "byline_female_test_fixture.json"
+      ))
+      @male_article = Article.new(File.join(
+        FIXTURES_DIR, "byline_male_test_fixture.json"
+      ))
+      @unknown_article = Article.new(File.join(
+        FIXTURES_DIR, "byline_unknown_test_fixture.json"
+      ))
+    }
+
+    it "should detect a female author" do
+      scores = @mn.process(@female_article)
+      scores[:result].should eq "Female"
+      scores[:counts][:male].round.should eq 0
+      scores[:counts][:female].round.should eq 1
+    end
+
+    it "should detect a male author" do
+      scores = @mn.process(@male_article)
+      scores[:result].should eq "Male"
+      scores[:counts][:male].round.should eq 1
+      scores[:counts][:female].round.should eq 0
+    end
+
+    it "should be unable to detect author" do
+      scores = @mn.process(@unknown_article)
+      scores[:result].should eq "Unknown"
+      scores[:counts][:male].should be_< 0.66
+      scores[:counts][:female].should be_< 0.66
+    end
+
+    it "should detect a female author from aux list" do
+      @female_article.byline("Anemona")
+      scores = @mn.process(@female_article)
+      scores[:result].should eq "Female"
+      scores[:counts][:male].should eq 0.0
+      scores[:counts][:female].should eq 1.0
+    end
+
+    it "should detect a male author from aux list" do
+      @male_article.byline("Kelefa")
+      scores = @mn.process(@male_article)
+      scores[:result].should eq "Male"
+      scores[:counts][:male].should eq 1.0
+      scores[:counts][:female].should eq 0.0
+    end
+
+    it "should fail to detect a name that doesn't exist in any list" do
+      @male_article.byline("jn323kjnbjt whwet")
+      scores = @mn.process(@male_article)
+      scores[:result].should eq "Unknown"
+      scores[:counts][:male].should eq 0.0
+      scores[:counts][:female].should eq 0.0
+    end
+
+  end
+end
--- src/metrics/byline_gender.rb
+++ src/metrics/byline_gender.rb
@@ -1,6 +1,108 @@
+require 'csv'
+
 module Metrics
   class BylineGender < Metrics::Default
     
-    
+    def initialize
+      @names = {
+        :male => read_names("male"),
+        :female => read_names("female")
+      }
+    end
+
+    class << self
+      def get_name
+        return "bylineGender"
+      end
+    end
+
+    def read_names(gender)
+      lang = @@config["lang"] || "EN"
+      country = @@config["country"] || "US"
+
+      count_file_name = gender + "_names_" + lang + "_" + country + ".csv"
+      definite_file_name = gender + "_auxilliary.csv"
+
+      count_data = {}
+      definite_data = []
+
+      CSV.open(
+        File.expand_path(
+          File.join(
+            File.dirname(__FILE__), 
+            "../../lib/metrics/names/#{count_file_name}"
+          )
+        ), 'r').to_a.each do |namePair|
+        count_data[namePair[0].downcase] = namePair[1].to_f
+      end
+
+      definite_data = CSV.open(
+        File.expand_path(
+          File.join(
+            File.dirname(__FILE__), 
+            "../../lib/metrics/names/#{definite_file_name}"
+          )
+        ),'r').to_a.flatten.compact.collect{|r| r.downcase } rescue []
+
+      return {
+        :counts => count_data,
+        :definite => definite_data
+      }
+
+    end
+
+    def process(article)
+      super(article)
+
+      score = { :result => "", :counts => 0 }
+
+      if (article.byline.is_a? String)
+        first_name = article.byline.split(" ")[0].downcase
+      else
+        first_name = article.byline[0].split(" ")[0].downcase
+      end
+
+      male = @names[:male][:counts][first_name] || 0
+      female = @names[:female][:counts][first_name] || 0
+      total = male + female
+
+      prob_male = 0
+      prob_female = 0
+
+      if (total > 0)
+        # compute probabilities      
+        prob_male = male / total.to_f if male
+        prob_female = female / total.to_f if female
+
+        score[:counts] = { :male => prob_male, :female => prob_female }
+      end
+
+      if (male > 0 && female > 0)  
+        if (prob_female > 0.66)
+          score[:result] = "Female"
+        elsif (prob_male > 0.66)
+          score[:result] = "Male"
+        else
+          score[:result] = "Unknown"
+        end
+      elsif (male > 0)
+        score[:result] = "Male"
+      elsif (female > 0)
+        score[:result] = "Female"
+      else
+        if (@names[:male][:definite].index(first_name))
+          score[:result] = "Male"
+          score[:counts] = { :male => 1.0, :female => 0.0 }
+        elsif (@names[:female][:definite].index(first_name))
+          score[:result] = "Female"
+          score[:counts] = { :male => 0.0, :female => 1.0 }
+        else
+          score[:result] = "Unknown"
+          score[:counts] = { :male => 0.0, :female => 0.0 }
+        end
+      end
+
+      score
+    end
   end
 end
