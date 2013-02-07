6f6e4c050e4f908d01a1be4a582e8fba938fcd59
Irene Ros <irene@bocoup.com>
2013-02-07 16:08:44 -0500

    fixed the byline gender to not break when we are faced with a null byline.

--- src/metrics/byline_gender.rb
+++ src/metrics/byline_gender.rb
@@ -23,59 +23,66 @@ module Metrics
 
       if (article.byline.is_a? String)
         first_name = article.byline.split(" ")[0].downcase
-      else
+      elsif (article.byline.is_a? Array)
         first_name = article.byline[0].split(" ")[0].downcase
+      else
+        first_name = nil
       end
 
       # try to retrieve from cache
-      if (@computed_names[first_name]) 
+      if (!first_name.nil?)
+        if (@computed_names[first_name]) 
 
-        @computed_names[first_name]
+          score = @computed_names[first_name]
 
-      else
-
-        male = @names[:male][:counts][first_name] || 0
-        female = @names[:female][:counts][first_name] || 0
-        total = male + female
-
-        prob_male = 0
-        prob_female = 0
-
-        if (total > 0)
-          
-          # compute probabilities      
-          prob_male = male / total.to_f if male
-          prob_female = female / total.to_f if female
-
-          score[:counts] = { :male => prob_male, :female => prob_female }
-        end
-
-        if (male > 0 && female > 0)  
-          if (prob_female > 0.66)
-            score[:result] = "Female"
-          elsif (prob_male > 0.66)
-            score[:result] = "Male"
-          else
-            score[:result] = "Unknown"
-          end
-        elsif (male > 0)
-          score[:result] = "Male"
-        elsif (female > 0)
-          score[:result] = "Female"
         else
-          if (@names[:male][:definite].index(first_name))
-            score[:result] = "Male"
-            score[:counts] = { :male => 1.0, :female => 0.0 }
-          elsif (@names[:female][:definite].index(first_name))
-            score[:result] = "Female"
-            score[:counts] = { :male => 0.0, :female => 1.0 }
-          else
-            score[:result] = "Unknown"
-            score[:counts] = { :male => 0.0, :female => 0.0 }
+
+          male = @names[:male][:counts][first_name] || 0
+          female = @names[:female][:counts][first_name] || 0
+          total = male + female
+
+          prob_male = 0
+          prob_female = 0
+
+          if (total > 0)
+            
+            # compute probabilities      
+            prob_male = male / total.to_f if male
+            prob_female = female / total.to_f if female
+
+            score[:counts] = { :male => prob_male, :female => prob_female }
           end
+
+          if (male > 0 && female > 0)  
+            if (prob_female > 0.66)
+              score[:result] = "Female"
+            elsif (prob_male > 0.66)
+              score[:result] = "Male"
+            else
+              score[:result] = "Unknown"
+            end
+          elsif (male > 0)
+            score[:result] = "Male"
+          elsif (female > 0)
+            score[:result] = "Female"
+          else
+            if (@names[:male][:definite].index(first_name))
+              score[:result] = "Male"
+              score[:counts] = { :male => 1.0, :female => 0.0 }
+            elsif (@names[:female][:definite].index(first_name))
+              score[:result] = "Female"
+              score[:counts] = { :male => 0.0, :female => 1.0 }
+            else
+              score[:result] = "Unknown"
+              score[:counts] = { :male => 0.0, :female => 0.0 }
+            end
+          end
+          # cache for future use
+          @computed_names[first_name] = score
         end
-        # cache for future use
-        @computed_names[first_name] = score
+      else
+        score[:result] = "Unknown"
+        score[:counts] = { :male => 0.0, :female => 0.0 }
       end
 
       article.add_metric "byline_gender" do
